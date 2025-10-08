d8b0fab7ad35a95f8415a21e7d0c1c0604c7f113
Ahmad Indra Nurfauzi <ahmad.nurfauzi@moladin.com>
2025-10-08 19:52:32 +0700

    add grpc smtp goggle

---
+++ internal/api/email.go
@@ -0,0 +1,45 @@
+package api
+
+import (
+	"context"
+	"ewallet-notification/cmd/proto/notification"
+	"ewallet-notification/helpers"
+	"ewallet-notification/internal/interfaces"
+	"ewallet-notification/internal/models"
+)
+
+type EmailAPI struct {
+	EmailService interfaces.IEmailService
+	notification.UnimplementedNotificationServiceServer
+}
+
+func (api *EmailAPI) SendEmail(ctx context.Context, req *notification.SendNotificationRequest) (*notification.SendNotificationResponse, error) {
+	var (
+		log = helpers.Logger
+	)
+
+	internalReq := models.InternalNotificationRequest{
+		TemplateName: req.TemplateName,
+		Recepient:    req.Recipient,
+		Placeholder:  req.Placeholders,
+	}
+
+	if err := internalReq.Validate(); err != nil {
+		log.Error("failed to validate request: ", err)
+		return &notification.SendNotificationResponse{
+			Message: "failed to validate request",
+		}, nil
+	}
+
+	err := api.EmailService.SendEmail(ctx, internalReq)
+	if err != nil {
+		log.Error("failed to send email: ", err)
+		return &notification.SendNotificationResponse{
+			Message: "failed to send email",
+		}, nil
+	}
+
+	return &notification.SendNotificationResponse{
+		Message: "success to send email",
+	}, nil
+}
